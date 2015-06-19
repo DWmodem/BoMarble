f65db5639cad437570b31abe9c46f220e0400222
Philippe Courtemanche <sigma.court@gmail.com>
2015-06-18 22:25:32 -0400

    Made lots of changes, wow!

---
+++ javascript/forestfire/forestFire.js
@@ -0,0 +1,26 @@
+//Forest fire
+
+//Region is the game board
+var region = [][];
+var REGION_WIDTH = 100;
+var REGION_HEIGHT = 100;
+
+
+function onUpdate(){
+	//Updating code
+}
+
+function render(){
+	//Displaying updated state
+}
+
+
+/*						*/
+/*	Tree object 		*/
+/*						*/
+function tree(positionX, positionY){
+	this.fireLevel = 0;
+
+}
+
+
